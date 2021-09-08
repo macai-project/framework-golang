b0da91f1ac85b073b3c35f85d65aef07bdc3ac3d
Mauro Artizzu <mauro.artizzu@macai.life>
2021-09-08 18:07:07 +0200

    fix: use go install

--- scripts/testwatch.sh
+++ scripts/testwatch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-go get -u github.com/radovskyb/watcher/...
-go get -u github.com/rakyll/gotest
+go install github.com/radovskyb/watcher/...
+go install github.com/rakyll/gotest
 watcher -cmd="gotest -race -count=1 -v -coverpkg=./... -coverprofile=coverage.out ./..." -dotfiles=false --keepalive -startcmd=true -ignore=coverage.out
