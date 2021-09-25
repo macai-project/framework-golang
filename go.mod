19bd082a511b01d942472c35135517ebd201f5d7
Alessandro <alessandro@macai.life>
2021-09-25 18:36:33 +0200

    feat(alessandro): improvements changed businssLogicHandler signature to have container loaded sentry directly in the framework

--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.26.0
+	github.com/aws/aws-sdk-go-v2 v1.9.0
 	github.com/aws/aws-sdk-go-v2/config v1.8.1
 	github.com/aws/aws-sdk-go-v2/service/eventbridge v1.7.0
+	github.com/getsentry/sentry-go v0.11.0
 	github.com/stretchr/testify v1.7.0
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
--- pkg/framework/framework.go
+++ pkg/framework/framework.go
@@ -3,15 +3,38 @@ package framework
 import (
 	"context"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/getsentry/sentry-go"
+	"github.com/macai-project/framework-golang/pkg/container"
+	"os"
+	"time"
 )
 
-var businessLogicHandler func(ctx context.Context, e events.CloudWatchEvent) (string, error)
+var c *container.Container
 
-func RegisterBusinessLogic(funzione func(ctx context.Context, e events.CloudWatchEvent) (string, error)) {
+var businessLogicHandler func(ctx context.Context, c *container.Container, e events.CloudWatchEvent) (string, error)
+
+func RegisterBusinessLogic(funzione func(ctx context.Context, c *container.Container, e events.CloudWatchEvent) (string, error)) {
 	businessLogicHandler = funzione
 }
 
 // HandleRequest avvia il framework
 func HandleRequest(ctx context.Context, e events.CloudWatchEvent) (string, error) {
-	return businessLogicHandler(ctx, e)
+	c = &container.Container{}
+
+	// Sentry
+	sentryDSN, _ := os.LookupEnv("SENTRY_DSN")
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              sentryDSN,
+		TracesSampleRate: 0.2,
+	})
+	if err != nil {
+		c.Logger.Fatalf("sentry.Init: %s", err)
+		return "sentry.Init", err
+	}
+	// Flush buffered events before the program terminates.
+	// Set the timeout to the maximum duration the program can afford to wait.
+	defer sentry.Flush(1 * time.Second)
+
+	result, err := businessLogicHandler(ctx, c, e)
+	return result, err
 }
