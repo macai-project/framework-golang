c6b191d2ee3ff97855eb949c97c7f6a8e8762056
Mauro Artizzu <mauro.artizzu@macaiapp.com>
2021-10-13 23:59:55 +0200

    feat: eventinput eventbridge helper

--- pkg/container/eventbridge.go
+++ pkg/container/eventbridge.go
@@ -1,10 +1,33 @@
 package container
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge/types"
 )
 
 // NewEventbridgeClient create a new Eventbridge client
 func (c *Container) NewEventbridgeClient() {
 	c.EventBridgeClient = eventbridge.NewFromConfig(c.awsConfig)
+}
+
+func (c *Container) EventInput(event interface{}, detailType string, eventBus string, source string, resources ...string) (*eventbridge.PutEventsInput, error) {
+
+	eventJson, err := json.Marshal(event)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling event: %s", err.Error())
+	}
+	return &eventbridge.PutEventsInput{
+		Entries: []types.PutEventsRequestEntry{
+			{
+				Detail:       aws.String(string(eventJson)),
+				DetailType:   aws.String(detailType),
+				EventBusName: aws.String(eventBus),
+				Resources:    resources,
+				Source:       aws.String(source),
+			},
+		},
+	}, nil
 }
