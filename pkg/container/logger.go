7ddca8516a077ddbdb837eb94ff8a31513fde2f3
Alessandro <alessandro@macai.life>
2021-10-19 11:31:06 +0200

    feat(alessandro): sentry hooked to zap

--- pkg/container/logger.go
+++ pkg/container/logger.go
@@ -1,15 +1,31 @@
 package container
 
 import (
+	"fmt"
+	"github.com/getsentry/sentry-go"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"os"
+	"time"
 )
 
 // NewLogger create a new Logger
 func (c *Container) NewLogger() {
-	logger, _ := zap.NewProduction()
+	logger, _ := zap.NewProduction(zap.Hooks(func(entry zapcore.Entry) error {
+		if entry.Level == zapcore.ErrorLevel {
+			defer sentry.Flush(2 * time.Second)
+			sentry.CaptureMessage(fmt.Sprintf("%s, Line No: %d :: %s", entry.Caller.File, entry.Caller.Line, entry.Message))
+		}
+		return nil
+	}))
 	if os.Getenv("LOG_LEVEL") == "debug" {
-		logger, _ = zap.NewDevelopment()
+		logger, _ = zap.NewDevelopment(zap.Hooks(func(entry zapcore.Entry) error {
+			if entry.Level == zapcore.ErrorLevel {
+				defer sentry.Flush(2 * time.Second)
+				sentry.CaptureMessage(fmt.Sprintf("%s, Line No: %d :: %s", entry.Caller.File, entry.Caller.Line, entry.Message))
+			}
+			return nil
+		}))
 	}
 	c.Logger = logger.Sugar()
 }
