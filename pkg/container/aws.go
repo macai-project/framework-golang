1fbdd1a039eda6991b03001c4766ffa40868f62e
Alessandro <alessandro@macai.life>
2021-10-30 19:13:11 +0200

    feat(alessandro): now we can use localstack

--- pkg/container/aws.go
+++ pkg/container/aws.go
@@ -2,13 +2,33 @@ package container
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"os"
 )
 
 // NewAWSConfig create a new AWS config object
 func (c *Container) NewAWSConfig() error {
 	var err error
-	c.awsConfig, err = config.LoadDefaultConfig(context.TODO())
+
+	// Create resolver for localstack if AWS_ENDPOINT and AWS_REGION are specified
+	customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+		awsEndpoint := os.Getenv("AWS_ENDPOINT")
+		awsRegion := os.Getenv("AWS_REGION")
+
+		if awsEndpoint != "" && awsRegion != "" {
+			return aws.Endpoint{
+				PartitionID:   "aws",
+				URL:           awsEndpoint,
+				SigningRegion: awsRegion,
+			}, nil
+		}
+
+		// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
+		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+	})
+
+	c.awsConfig, err = config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolver(customResolver))
 	if err != nil {
 		return err
 	}
