acb243f8229b8b31a4eb9f8b2197b704d16d3ab9
Alessandro <alessandro@macai.life>
2021-10-30 19:11:31 +0200

    Revert "feat(alessandro): use a generic interface for the eventbridge client to be able to mock it up"
    This reverts commit fdbf1fb2bf4ccde67c9b6b0e8ec72598f5827339.

--- pkg/container/struct.go
+++ pkg/container/struct.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"go.uber.org/zap"
@@ -14,7 +15,7 @@ type Container struct {
 	awsConfig            aws.Config
 	DB                   *sql.DB
 	CloudwatchClient     *cloudwatch.Client
-	EventBridgeClient    eventClient
+	EventBridgeClient    *eventbridge.Client
 	S3Client             *s3.Client
 	Logger               *zap.SugaredLogger
 	SecretsManagerClient *secretsmanager.Client
