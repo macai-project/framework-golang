bb302c425ee1612e672d3c9b2e99c77087dad152
Mauro Artizzu <mauro.artizzu@macaiapp.com>
2022-02-17 21:55:14 +0100

    feat: cloudsearch client

---
+++ pkg/container/cloudsearch.go
@@ -0,0 +1,12 @@
+package container
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/cloudsearchdomain"
+)
+
+// NewCloudsearchClient create a new Cloudsearch client
+func (c *Container) NewCloudsearchClient() {
+	if c.CloudsearchClient == nil {
+		c.CloudsearchClient = cloudsearchdomain.NewFromConfig(c.AwsConfig)
+	}
+}
