2a43d79d9ca747fb1053cf97e0a7fdce3805bdd6
Alessandro Lattao <alessandro@macaiapp.com>
2022-05-19 00:30:24 +0200

    refactor+fix: divide handlers by file and add api gateway request handler

---
+++ pkg/framework/handler_s3.go
@@ -0,0 +1,78 @@
+package framework
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-xray-sdk-go/instrumentation/awsv2"
+	"github.com/macai-project/framework-golang/pkg/container"
+	"os"
+	"strconv"
+	"time"
+)
+
+var businessLogicHandlerS3 func(ctx context.Context, c *container.Container, e events.S3Event) (string, error)
+
+func RegisterBusinessLogicS3(f func(ctx context.Context, c *container.Container, e events.S3Event) (string, error)) {
+	businessLogicHandlerS3 = f
+}
+
+func HandleRequestS3(ctx context.Context, e events.S3Event) (string, error) {
+	var err error
+
+	// Check if the container has been initialized
+	if c == nil {
+		return "", fmt.Errorf("container struct must be initialized and passed to the framework")
+	}
+
+	// Logger
+	c.NewLogger()
+	defer c.Logger.Sync()
+
+	// AWS Config
+	err = c.NewAWSConfig(ctx)
+	if err != nil {
+		return "error initializing AWS Config", err
+	}
+
+	// Sentry
+	sentryDSN, ok := os.LookupEnv("SENTRY_DSN")
+	if ok != true {
+		c.Logger.Warnf("error fetching SENTRY_DSN, sentry won't sample")
+		sentryDSN = ""
+	}
+
+	samplingRate, err := strconv.ParseFloat(os.Getenv("SENTRY_SAMPLING_RATE"), 64)
+	if err != nil {
+		c.Logger.Warnf("error converting SENTRY_SAMPLING_RATE to float64, set to 1.0")
+		samplingRate = 1.0
+	}
+
+	environment, ok := os.LookupEnv("ENVIRONMENT")
+	if ok != true {
+		c.Logger.Warnf("error fetching ENVIRONMENT, setting empty environment")
+		environment = ""
+	}
+
+	err = sentry.Init(sentry.ClientOptions{
+		Dsn:              sentryDSN,
+		Environment:      environment,
+		TracesSampleRate: samplingRate,
+	})
+
+	// Xray
+	awsv2.AWSV2Instrumentor(&c.AwsConfig.APIOptions)
+
+	if err != nil {
+		return "error in sentry.Init", err
+	}
+	// Flush buffered events before the program terminates.
+	// Set the timeout to the maximum duration the program can afford to wait.
+	defer sentry.Flush(1 * time.Second)
+
+	result, err := businessLogicHandlerS3(ctx, c, e)
+	if err != nil {
+		sentry.CaptureException(err)
+	}
+	return result, err
+}
