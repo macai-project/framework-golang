15fdad6e04ed2773f86f446c2bf90adc63a4d951
Giovanni Giordano <giovanni.giordano@macaiapp.com>
2022-07-20 12:39:28 +0200

    feat: add signer to appsync client

--- pkg/container/appsync.go
+++ pkg/container/appsync.go
@@ -1,13 +1,21 @@
 package container
 
 import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
 	appsync "github.com/sony/appsync-client-go"
 	"github.com/sony/appsync-client-go/graphql"
 )
 
 // NewAppSyncClient creates a new AppSyncClient client
-func (c *Container) NewAppSyncClient(url string) {
+func (c *Container) NewAppSyncClient(url string, region string) {
 	if c.AppSyncClient == nil {
-		c.AppSyncClient = appsync.NewClient(appsync.NewGraphQLClient(graphql.NewClient(url)))
+		sess := session.Must(session.NewSession())
+		signer := v4.NewSigner(sess.Config.Credentials)
+
+		c.AppSyncClient = appsync.NewClient(
+			appsync.NewGraphQLClient(graphql.NewClient(url)),
+			appsync.WithIAMAuthorization(*signer, region, url),
+		)
 	}
 }
