061cb91316d10c20aa73bb68f607f26562a33e3c
Lorenzo Zamponi <lorenzo@macaiapp.com>
2022-11-25 12:44:10 +0100

    feat: send events in batch

--- pkg/container/eventbridge.go
+++ pkg/container/eventbridge.go
@@ -1,6 +1,7 @@
 package container
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -32,4 +33,55 @@ func (c *Container) EventInput(event interface{}, detailType string, eventBus st
 			},
 		},
 	}, nil
+}
+
+func (c *Container) SendBatchEvents(ctx context.Context, events []interface{}, detailType string, eventBus string, source string, batchSize int) error {
+	var entries []types.PutEventsRequestEntry
+	counterSize := 0
+
+	for i, event := range events {
+		entry, err := c.EventRequestEntry(event, detailType, eventBus, source)
+		if err != nil {
+			return err
+		}
+
+		entries = append(entries, entry)
+
+		if i-counterSize >= batchSize {
+			// Limit of batch size reached, send event
+			counterSize = counterSize + i
+			_, err = c.EventBridgeClient.PutEvents(ctx, &eventbridge.PutEventsInput{Entries: entries})
+			if err != nil {
+				return fmt.Errorf("error in Eventbridge SendBatchEvents: %s", err.Error())
+			}
+			entries = []types.PutEventsRequestEntry{}
+		}
+	}
+
+	if len(entries) > 0 {
+		_, err := c.EventBridgeClient.PutEvents(ctx, &eventbridge.PutEventsInput{Entries: entries})
+		if err != nil {
+			return fmt.Errorf("error in Eventbridge SendBatchEvents: %s", err.Error())
+		}
+	}
+	return nil
+}
+
+func (c *Container) EventRequestEntry(event interface{}, detailType string, eventBus string, source string, resources ...string) (types.PutEventsRequestEntry, error) {
+	var requestEntry types.PutEventsRequestEntry
+
+	eventJson, err := json.Marshal(event)
+	if err != nil {
+		return requestEntry, fmt.Errorf("error unmarshaling event: %s", err.Error())
+	}
+
+	requestEntry = types.PutEventsRequestEntry{
+		Detail:       aws.String(string(eventJson)),
+		DetailType:   aws.String(detailType),
+		EventBusName: aws.String(eventBus),
+		Resources:    resources,
+		Source:       aws.String(source),
+	}
+
+	return requestEntry, nil
 }
